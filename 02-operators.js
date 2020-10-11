4719958b23052ec9da7b3350eb6aa6d1ab1be309
rnwathon <rakhmatnashrul@gmail.com>
2020-10-11 18:11:08 +0800

    Operators

---
+++ 02-operators.js
@@ -0,0 +1,95 @@
+// Operators
+// adalah sesuatu yang melakukan sesutu terhadap sesuatu dan menghasilkan sesuatu.
+
+// 1. Arithmetic
+// 2. Assignment
+// 3. Comparison
+// 4. Logical
+
+// 1. Arithmetic
+// =============
+// Addition (+)
+// Subtraction (-)
+// Multiplication (*)
+// Division (/)
+// Modulus (%)
+// Increment (++)
+// Decrement (--)
+
+// let num = 1
+// num++
+// console.log(num)
+
+// WTF JavaScript
+
+// 2. Assignment
+// -------------
+// =
+// +=
+// -=
+// *=
+// /=
+
+// let numAssignment = 2;
+// console.log(numAssignment)
+// numAssignment += 2
+// numAssignment = numAssignment + 2
+// console.log(numAssignment)
+
+// 3. Comparison
+// -------------
+// == (equal)
+// === (equal but strictly type)
+// > (more than)
+// >= (more than equal)
+// < (less than)
+// <= (less than equal)
+// != (not equal)
+// !== (not equal but strictly type)
+
+// console.log(8 > 4)
+
+// 4. Logical
+// ----------
+// && (AND)
+// || (OR)
+// ! (NOT)
+
+// Logic Table
+// ------------
+// && (AND)
+//    A   |   B   |  Result  //
+//  true  | true  |  true
+//  true  | false |  false
+//  false | true  |  false
+//  false | false |  false
+
+// || (AND)
+//    A   |   B   |  Result  //
+//  true  | true  |  true
+//  true  | false |  true
+//  false | true  |  true
+//  false | false |  false
+
+// ! (NOT)
+//    A   | Result  //
+//  true  | false
+//  false | true
+
+// notes: Hasil dari Comparison operator & Logical operator adalah boolean
+
+// Thruthy
+// - String that not empty
+// - Number besides 0
+// - Array (bahkan array kosong)
+// - Object (bahkan object kosong)
+
+// False
+// - String kosong
+// - 0 (nol)
+// - null
+// - undefined
+
+// Cara ngecek truthy atau falsy -> pake !!
+// console.log(!!{})
+
