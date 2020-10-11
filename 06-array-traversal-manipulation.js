9e49c9b111100fe1dba272f3d96f6fe5d1c6bb48
rnwathon <rakhmatnashrul@gmail.com>
2020-10-11 18:11:50 +0800

    array

---
+++ 06-array-traversal-manipulation.js
@@ -0,0 +1,29 @@
+// Array Traveral & Manipulation
+
+let arr = [1,2,3,5,7,10,7];
+// console.log(arr[2])
+// console.log(arr.length)
+
+// for(let i=0; i < arr.length; i++){
+//   console.log(arr[i])
+// }
+
+// Mengubah data di dalam array berdasarkan index
+// arr[1] = "Hello"
+// console.log(arr)
+
+// Menambahkan data baru
+// arr[7] = "hello"
+// console.log(arr)
+
+// Array method ang wajib tau
+// .push
+// .pop
+// .shift
+// .unshift
+// .slice
+// .splice
+
+// arr.push("world")
+// console.log(arr)
+
