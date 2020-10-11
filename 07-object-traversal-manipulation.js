75a6bbb6de8fbc90f1f583914c052f38cb16d96e
rnwathon <rakhmatnashrul@gmail.com>
2020-10-11 18:12:00 +0800

    object

---
+++ 07-object-traversal-manipulation.js
@@ -0,0 +1,27 @@
+// Object traversal & manipulation
+
+let obj = {
+  nama: "John Doe",
+  umur: 18,
+  domisili: "asgard"
+}
+
+// console.log(obj.nama)
+// console.log(obj["nama"])
+
+// obj.nama = "Thor"
+// obj.skill = "Thunder"
+
+// console.log(obj)
+
+// Object Method
+// -------------
+// Object.keys(obj)
+// Object.values(obj)
+// Object.entries(obj)
+
+// console.log(Object.keys(obj), "KEYS")
+// console.log(Object.values(obj), "VALUES")
+// console.log(Object.entries(obj), "ENTRIES")
+
+
