71475ebf04513ace981f92008252b1d1cad55936
rnwathon <rakhmatnashrul@gmail.com>
2020-10-11 18:11:30 +0800

    looping

---
+++ 04-looping.js
@@ -0,0 +1,18 @@
+// Looping
+// adalah hal yang bisa digunakan ketika kita ingin
+// melakukan hal berulang-ulang.
+// Halnya bisa hal yang sama atau yang ber-pola.
+
+// FOR LOOP
+// --------
+// Anatomy:
+// for(deklarasi; kondisi; increment/decrement){
+  // Blok kode yang akan di ulang
+  // berdasarkan kondisi
+// }
+
+// for(let i = 5; i > 3; i--){
+//   // Loop code here
+//   console.log(i)
+// }
+
