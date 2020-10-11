1c779752557f397a1708395255fddc4d017415e0
rnwathon <rakhmatnashrul@gmail.com>
2020-10-11 18:11:19 +0800

    Conditinals

---
+++ 03-conditionals.js
@@ -0,0 +1,16 @@
+// Conditionals
+// adalah hal yang menentukan aturan di program kita.
+
+// IF statement
+// let str1 = "hello";
+// let str2 = "world";
+
+// let umur = "hello";
+
+// if(umur <= 10){
+//   console.log("anak-anak")
+// } else if(umur > 10){
+//   console.log("remaja")
+// } else {
+//   console.log("dewasa")
+// }
